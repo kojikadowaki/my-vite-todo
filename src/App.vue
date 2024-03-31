7a5b843841bb41cae5659b01d8f8de0e4688be2a
kojikaodwaki <gbsg9405@yahoo.co.jp>
2024-03-31 21:56:21 +0900

    TODOレイアウト

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<template>
+  <div class="wrap">
+    <TheHeader/><!-- コンポーネントしたヘッダーを読み込み -->
+    <main class="main">メイン</main>
+    <TheFooter/><!-- コンポーネントしたフッターを読み込み -->
+  </div>
+</template>
+ 
+<style>
+@import 'assets/css/destyle.css';
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 370px;
+  margin: 0 auto;
+  font-family: sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+ 
+.main {
+flex: 1;
+width: 100%;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
+ 
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+<footer class="footer">
+  <small>&copy;my-vie-todo</small>
+</footer>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 15px;
+  height: 30px;
+}
+
+</style>
